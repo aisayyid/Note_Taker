a29afd8a8ee6693b8f0a2004385a431caa318a87
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-13 17:50:49 -0400

    Settign up server with express, a port listen, and makign express handle parsing.

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+//Setting required global variables.
 const express = require ("express");
 const { v1: uuidv1 } = require('uuid');
 const fs = require("fs");
+
+//Using express.
+const app = express();
+//Setting initial port.
+var PORT = process.env.PORT || 8080;
+//Setting up express to handle data parsing.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
