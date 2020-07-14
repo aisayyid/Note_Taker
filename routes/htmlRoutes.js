78efb08f58c8e0afb4d9503bfd6d705f65933108
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-13 20:25:12 -0400

    Set up html routing.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,16 @@
 // We need to include the path package to get the correct file path for our html
 const path = require("path");
+
+module.exports = function(app) {
+
+  //HTML GET requests that direct users to a page.
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+  
+
+    // If a matching route isn't found default to index.html
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+  };
