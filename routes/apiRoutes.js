1bc5337b4f232df0c29109dd1f8a5d7bdcdea07d
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-13 20:17:28 -0400

    Set up a post to send new notes data to the client side.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,15 @@
 const notesdb = require("../db/Notesdb");
 
 module.exports = function(app) {
-
+//API GET Request.  Handles data coming in from notes.html
     app.get("/api/notes", function(req, res) {
         res.json("db/db.json");
       });
-    
+//Saves new notes to db.json file.
+      app.post("/api/notes", function (req,res) {
+        notesdb.push(req.body);
+        res.json(true);
+
+      })
      
-
-
 }
